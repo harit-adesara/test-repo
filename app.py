e2dd9acd659fe31e51598c4c49daa4e48d400186
Harit-01 <harit13soni@gmail.com>
2026-07-08 13:14:20 +0530

    test

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
-import requests
+def calculate_area(radius):
+    return math.pi * radius ** 2
 
-app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "ok"}
+def print_result():
+    area = calculate_areaa(5)
+    print(f"Area: {area}")
+
+
+if __name__ == "__main__":
+    print_result()
